679832e9b85123f4834b58d438f3f959eb508708
Samber9891 <Slem0119@gmail.com>
2020-10-24 15:08:55 -0400

    updated switch statement

--- GradeSystemSwitch.java
+++ GradeSystemSwitch.java
@@ -7,27 +7,27 @@ import java.util.Scanner;
 public class GradeSystemSwitch {
 
     public static void main(String[] args) {
-        
+
        Scanner input = new Scanner(System.in); 
         System.out.println("Please enter students grade score.");  
         
-        int gradeScore = input.nextInt(); 
+        Integer gradeScore = input.nextInt();
         
-    switch (true) {
-        case (gradeScore >=90 && gradeScore <= 100) : 
+        switch (gradeScore) {
+        case 100,99,98,97,96,95,94,93,92,91,90 : 
             System.out.println("A");
         break;
-        case (gradeScore >=80 && gradeScore < 90): 
+        case 89,88,87,86,85,84,83,82,81,80: 
             System.out.println("B");
         break;
-        case (gradeScore >=70 && gradeScore < 80) :
+        case 79,78,76,75,74,73,72,71,70 :
             System.out.println("C");
-        case (gradeScore >=60 && gradeScore < 70):
+            break;
+        case 69,68,67,66,65,64,63,62,61,60:
             System.out.println("D");
-        case (gradeScore >=0 && gradeScore < 65):
-            System.out.println("F");
+            break; 
         default: 
-        System.out.println("Please enter between 1 and 100");
+    System.out.println("F");
 
 input.close();
         }
